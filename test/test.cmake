99d690cc4cbeaef5e3d7f4b41dafeb1676b543ee
creeper5820 <cqq1960180796@163.com>
2026-04-10 13:18:50 +0800

    ci: decouple Lua tests from ROS build environment
    Add standalone test/CMakeLists.txt so CI can run Lua tests via
    cmake -S test without sourcing ROS or resolving rclcpp.

--- test/test.cmake
+++ test/test.cmake
@@ -1,5 +1,8 @@
 find_program(LUA_EXECUTABLE NAMES lua lua5.4 REQUIRED)
 
+# Resolve project root regardless of where this file is included from
+get_filename_component(RMCS_NAVIGATION_ROOT "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
+
 set(RMCS_NAVIGATION_LUA_TESTS
     clock
     fsm
@@ -8,10 +11,9 @@ set(RMCS_NAVIGATION_LUA_TESTS
 )
 
 foreach(test_name IN LISTS RMCS_NAVIGATION_LUA_TESTS)
-    set(LUA ${LUA_EXECUTABLE})
     add_test(
         NAME rmcs-navigation.lua.${test_name}
-        COMMAND ${LUA} ${CMAKE_CURRENT_SOURCE_DIR}/test/lua/${test_name}.lua
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        COMMAND ${LUA_EXECUTABLE} ${RMCS_NAVIGATION_ROOT}/test/lua/${test_name}.lua
+        WORKING_DIRECTORY ${RMCS_NAVIGATION_ROOT}
     )
 endforeach()
